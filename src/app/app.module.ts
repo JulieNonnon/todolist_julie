46b54228d1e36d13540b240d218d27a197c5617a
JulieNonnon <julie.nonnon@gmail.com>
2023-03-17 10:58:31 +0100

    [FEATURE] création bouton créer une tâche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CreateTaskButtonComponent } from './components/create-task-button/create-task-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreateTaskButtonComponent
   ],
   imports: [
     BrowserModule,
