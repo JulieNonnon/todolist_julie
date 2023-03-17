9a12930a40a683128dd195aee7a8531c68135615
JulieNonnon <118167349+JulieNonnon@users.noreply.github.com>
2023-03-17 11:50:00 +0100

    Revert "Feat/validate task button"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CreateTaskButtonComponent } from './components/create-task-button/create-task-button.component';
-import { ModifyTaskButtonComponent } from './components/modify-task-button/modify-task-button.component';
-import { ValidateTaskButtonComponent } from './components/validate-task-button/validate-task-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    CreateTaskButtonComponent,
-    ModifyTaskButtonComponent,
-    ValidateTaskButtonComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
